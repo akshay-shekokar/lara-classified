2618793b540bd92b644f4f2cf992942947ab86e7
Akshay Shekokar <akshay.shekokar@gmail.com>
2020-10-03 22:44:05 +0530

    product details page responsive

--- src/containers/item-details-page/item-details-page.jsx
+++ src/containers/item-details-page/item-details-page.jsx
@@ -8,12 +8,15 @@ import {
   productDetails,
   productOwnerDetails,
 } from "../../dummy-data/productDetails";
+import "./item-details-page.css";
 
 const ItemDetailPage = () => {
   return (
     <div className="body-margin">
-      <ProductDetailsContainer details={productDetails} />
-      <ProductOwnerDetails owner={productOwnerDetails} />
+      <div className="ak-item-and-owner-detail">
+        <ProductDetailsContainer details={productDetails} />
+        <ProductOwnerDetails owner={productOwnerDetails} />
+      </div>
       <SimilarAds />
     </div>
   );
