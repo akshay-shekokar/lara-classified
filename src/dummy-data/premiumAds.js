ef6f22ce049baddb7a9b9845ebed0ccaec423ee2
akshay-shekokar <akshay.shekokar@gmail.com>
2021-01-09 15:46:28 +0530

    added images to the application

--- src/dummy-data/premiumAds.js
+++ src/dummy-data/premiumAds.js
@@ -2,7 +2,7 @@ export const premiumAds = [
   {
     id: 1,
     imageUrl:
-      "https://laraclassified.bedigit.com/storage/files/us/2628/thumb-320x240-2bf48016b7c4942b83554c73420f98ef.jpg?v=1",
+      "images/item.jpg",
     title: "Mobile",
     rating: 4,
     reviews: 10,
@@ -11,7 +11,7 @@ export const premiumAds = [
   {
     id: 2,
     imageUrl:
-      "https://laraclassified.bedigit.com/storage/files/us/1694/thumb-320x240-9998e7e5a7c6b728fa14b8a161bf82f9.jpg?v=1",
+      "images/item.jpg",
     title: "Mobile",
     rating: 4,
     reviews: 10,
@@ -20,7 +20,7 @@ export const premiumAds = [
   {
     id: 3,
     imageUrl:
-      "https://laraclassified.bedigit.com/storage/files/us/12325/thumb-320x240-67a742e95dc632858af2027eb15ae98d.jpg?v=1",
+      "images/item.jpg",
     title: "Mobile",
     rating: 4,
     reviews: 10,
@@ -29,7 +29,7 @@ export const premiumAds = [
   {
     id: 4,
     imageUrl:
-      "https://laraclassified.bedigit.com/storage/files/us/4062/thumb-320x240-7ba89a567ca2975ab4b03b600f305cd1.jpg?v=1",
+      "images/item.jpg",
     title: "Mobile",
     rating: 4,
     reviews: 10,
@@ -38,7 +38,7 @@ export const premiumAds = [
   {
     id: 5,
     imageUrl:
-      "https://laraclassified.bedigit.com/storage/files/us/2800/thumb-320x240-2b74c8d0439028ad8da542988d6aa55d.jpg?v=1",
+      "images/item.jpg",
     title: "Mobile",
     rating: 4,
     reviews: 10,
